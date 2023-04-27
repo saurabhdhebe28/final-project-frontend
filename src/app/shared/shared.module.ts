813360914f7b2330ba00b3f02907ed308c7b8634
Bushra0505 <bushra.s.khan@vernost.in>
2023-04-27 17:07:22 +0530

    added voucher and offers dropdown

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,20 +5,20 @@ import { ButtonComponent } from './components/button/button.component';
 import { FormsModule } from '@angular/forms';
 import { NavComponent } from './components/nav/nav.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-
+import { DropdownComponent } from './components/dropdown/dropdown.component';
 
 
 @NgModule({
   declarations: [
     InputComponent,
-    ButtonComponent,NavComponent,SidebarComponent
+    ButtonComponent,NavComponent,SidebarComponent, DropdownComponent
   ],
   imports: [
     CommonModule,FormsModule
   ],
   exports: [
     InputComponent,
-    ButtonComponent,NavComponent,SidebarComponent
+    ButtonComponent,NavComponent,SidebarComponent, DropdownComponent
   ],
 })
 export class SharedModule { }
